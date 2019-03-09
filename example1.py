ef4cfba8241104007a83784eab4f4d1f0db8ba93
lvannoli <48384537+lvannoli@users.noreply.github.com>
2019-03-09 17:26:12 +0100

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,32 @@
+#this is my first program 
+a = 2.3
+b = 4.5
+c = a/b
+#plain print
+print (a,b,c)
+
+#plain print of more vbariables
+print ('c = a/b = ', c)
+
+#print using ""
+print ("a = {0}, b = {1}, c = {2}".format( a, b, c) )
+
+#print using ''
+print ('a = {0}, b = {1}, c={2}'.format( a, b, c) )
+
+print ("fine esempio 1")
+
+#esempio 2
+print ("inizio esempio 2")
+
+z = int(input("insert a number: "))
+print ("your imput: ", z)
+if z>0:
+	print ("positive number: ", z)
+else: 
+	print ("negative number: ", z)
+
+#esempio 3
+print ("inizio esempio 3")
+
+
